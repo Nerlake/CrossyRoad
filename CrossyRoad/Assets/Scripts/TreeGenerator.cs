d1342136cc74e6d676243334531029d55fd8f9f1
Léo HOTZ <cdf.leo.hotz@gmail.com>
2024-03-04 15:55:55 +0100

    Feat: ajout du prefab "Herbe"

---
+++ CrossyRoad/Assets/Scripts/TreeGenerator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject treePrefab;
+    [SerializeField] private int minPosition;
+    [SerializeField] private int maxPosition;
+    [SerializeField] private int treeCount;
+
+
+    void Start()
+    {
+        for(int i=0; i<treeCount; i++)
+        {
+            float randomZ = Random.Range(minPosition, maxPosition);
+            Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, randomZ);
+            Instantiate(treePrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+}
