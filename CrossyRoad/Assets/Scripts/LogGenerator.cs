be77110b8d0d0c501eaed9218633bfcf76527701
Tanguy <pierrontanguy@gmail.com>
2024-03-15 22:11:50 +0100

    Added start of log spawn logic

--- CrossyRoad/Assets/Scripts/LogGenerator.cs
+++ CrossyRoad/Assets/Scripts/LogGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class LogGenerator : MonoBehaviour
@@ -8,27 +9,29 @@ public class LogGenerator : MonoBehaviour
     [SerializeField] private GameObject logPrefab;
     [SerializeField] private int minPosition;
     [SerializeField] private int maxPosition;
-    [SerializeField] private int logCount;
+    [SerializeField] private int logDelay;
+    [SerializeField] private int logSpeed;
     private bool isRightSpawner;
 
+    System.Random random = new System.Random();
     void Start()
     {
-        System.Random random = new System.Random();
-
         isRightSpawner = random.Next(0, 2) == 1;
-        for (int i=0; i<logCount; i++)
+    }
+    
+    private void Update()
+    {        
+        double delay = random.Next(0,10000);
+
+        while(delay < logDelay)
         {
-            float startingPosition;
-            if(isRightSpawner)
-            {
-                startingPosition = maxPosition;
-            }
-            else
-            {
-                startingPosition = minPosition;
-            }
-            Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, startingPosition);
-            Instantiate(logPrefab, spawnPosition, Quaternion.identity);
+            Thread.Sleep(300);
+            delay*=1.2;
         }
+
+        float startingPosition = isRightSpawner ? maxPosition : minPosition;
+
+        Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, startingPosition);
+        Instantiate(logPrefab, spawnPosition, Quaternion.identity);
     }
 }
