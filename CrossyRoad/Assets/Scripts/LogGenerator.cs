eea09ac0f7ca093769de3c0a174dfafb7567fb0a
Tanguy <pierrontanguy@gmail.com>
2024-03-16 08:59:24 +0100

    WOrkend on log movement logic

--- CrossyRoad/Assets/Scripts/LogGenerator.cs
+++ CrossyRoad/Assets/Scripts/LogGenerator.cs
@@ -11,6 +11,7 @@ public class LogGenerator : MonoBehaviour
     [SerializeField] private int maxPosition;
     [SerializeField] private int logDelay;
     [SerializeField] private int logSpeed;
+    private List<GameObject> logList = new List<GameObject>();
     private bool isRightSpawner;
 
     System.Random random = new System.Random();
@@ -21,6 +22,10 @@ public class LogGenerator : MonoBehaviour
     
     private void Update()
     {        
+        foreach(GameObject log in logList)
+        {
+            log.transform.position += isRightSpawner ? new Vector3(0, -logSpeed, 0) : new Vector3(0, logSpeed, 0);
+        }
         double delay = random.Next(0,10000);
 
         while(delay < logDelay)
@@ -32,6 +37,6 @@ public class LogGenerator : MonoBehaviour
         float startingPosition = isRightSpawner ? maxPosition : minPosition;
 
         Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, startingPosition);
-        Instantiate(logPrefab, spawnPosition, Quaternion.identity);
+        logList.Add(Instantiate(logPrefab, spawnPosition, Quaternion.identity));
     }
 }
