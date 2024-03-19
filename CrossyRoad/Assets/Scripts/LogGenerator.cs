c2f8b13cae3034f21d8c24f19ae155d7e5e54bc0
Tanguy <pierrontanguy@gmail.com>
2024-03-19 15:49:30 +0100

    Added lillypad

--- CrossyRoad/Assets/Scripts/LogGenerator.cs
+++ CrossyRoad/Assets/Scripts/LogGenerator.cs
@@ -7,36 +7,56 @@ using UnityEngine;
 public class LogGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject logPrefab;
-    [SerializeField] private int minPosition;
-    [SerializeField] private int maxPosition;
+    [SerializeField] private GameObject lilypadPrefab;
+    private float minPosition;
+    private float maxPosition;
     [SerializeField] private int logDelay;
-    [SerializeField] private int logSpeed;
+    private float logSpeed;
+    private bool isLilySpawner;
     private List<GameObject> logList = new List<GameObject>();
     private bool isRightSpawner;
 
     System.Random random = new System.Random();
     void Start()
     {
+        float startingPosition;
+        logSpeed = random.Next(1,5)/20f;
+        minPosition = this.transform.position.z - 25;
+        maxPosition = this.transform.position.z + 25;
         isRightSpawner = random.Next(0, 2) == 1;
+        isLilySpawner = random.Next(0,2) == 0;
+
+        if (isLilySpawner)
+        {
+            startingPosition = random.Next(15, 35);
+            Vector3 spawnPosition = new Vector3(this.transform.position.x, this.transform.position.y, startingPosition);
+            Instantiate(lilypadPrefab, spawnPosition, Quaternion.identity);
+        }
+        else
+        {
+            startingPosition = isRightSpawner ? maxPosition : minPosition;
+
+            Vector3 spawnPosition = new Vector3(this.transform.position.x, this.transform.position.y, startingPosition);
+            logList.Add(Instantiate(logPrefab, spawnPosition, Quaternion.identity));
+        }
+
+        
     }
     
     private void Update()
-    {        
+    {
+        if (isLilySpawner)
+        {
+            return;
+        }
+
         foreach(GameObject log in logList)
         {
-            log.transform.position += isRightSpawner ? new Vector3(0, -logSpeed, 0) : new Vector3(0, logSpeed, 0);
-        }
-        double delay = random.Next(0,10000);
-
-        while(delay < logDelay)
-        {
-            Thread.Sleep(300);
-            delay*=1.2;
-        }
-
-        float startingPosition = isRightSpawner ? maxPosition : minPosition;
-
-        Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, startingPosition);
-        logList.Add(Instantiate(logPrefab, spawnPosition, Quaternion.identity));
+            log.transform.position += isRightSpawner ? new Vector3(0, 0, -logSpeed) : new Vector3(0,0, logSpeed);
+            if(log.transform.position.z > maxPosition || log.transform.position.z < minPosition)
+            {
+                Destroy(log);
+            }
+        }     
     }
 }
