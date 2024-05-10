4e4d17cae080a5ddddef309d0dc45602775aaef4
Fatih BOZLAK <bozlak.fatih@gmail.com>
2024-05-10 14:39:09 +0200

    Player:   - déplacement du joueur dans les 4 directions d'une unitée ;
      - lors de déplacement il y a des nombres flottants qu'il faut arrondir ;

---
+++ Assets/Player/PlayerMovements.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovements : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            MoveForward();
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            MoveForward();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            MoveForward();
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            MoveForward();
+        }
+    }
+
+    private void MoveForward()
+    {
+        transform.Translate(Vector3.forward);
+    }
+}
