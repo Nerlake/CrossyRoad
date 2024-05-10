277048bcd09c418106d40f3ed67145cb2bb4c703
Fatih BOZLAK <bozlak.fatih@gmail.com>
2024-05-10 16:33:28 +0200

    Player:   - animation pour le déplacement du joueur de manière programmatique et non en utilisant l'Animator ;
      - Peut-être à factoriser les deux méthodes Up et Down ;

--- Assets/Player/PlayerMovements.cs
+++ Assets/Player/PlayerMovements.cs
@@ -1,12 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerMovements : MonoBehaviour
 {
+    private bool isAnimationUpInProgress = false;
+    private bool isAnimationDownInProgress = false;
+
+    private float speed = 20f;
+    private float heightUp = 1f;
+    private float interpolation;
+
+    private Vector3 startPositionAnimation;
+    private Vector3 endPositionAnimation;
+
     private void Update()
     {
+        if (isAnimationUpInProgress)
+        {
+            MoveWithAnimationUp();
+            return;
+        }
+
+        KeyboardController();
+    }
+
+    private void KeyboardController()
+    {
+        if (isAnimationDownInProgress)
+        {
+            MoveWithAnimationDown();
+            return;
+        }
+
         if (Input.GetKeyDown(KeyCode.W))
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
@@ -31,7 +55,47 @@ public class PlayerMovements : MonoBehaviour
 
     private void MoveForward()
     {
-        transform.Translate(Vector3.forward);
+        if (!isAnimationUpInProgress && !isAnimationDownInProgress)
+        {
+            startPositionAnimation = transform.position;
+            endPositionAnimation = startPositionAnimation + transform.forward * 0.5f + Vector3.up * heightUp;
+            isAnimationUpInProgress = true;
+        }
+    }
+
+    private void MoveWithAnimationUp()
+    {
+        if (interpolation >= 1 && isAnimationUpInProgress)
+        {
+            isAnimationUpInProgress = false;
+            isAnimationDownInProgress = true;
+            startPositionAnimation = transform.position;
+            endPositionAnimation += transform.forward * 0.5f + Vector3.down * heightUp;
+            interpolation = 0;
+            return;
+        }
+
+        interpolation += speed * Time.deltaTime;
+        transform.position = Vector3.Lerp(startPositionAnimation, endPositionAnimation, interpolation);
+    }
+
+    private void MoveWithAnimationDown()
+    {
+        if (interpolation >= 1 && isAnimationDownInProgress)
+        {
+            isAnimationUpInProgress = false;
+            isAnimationDownInProgress = false;
+            interpolation = 0;
+            roundPosition();
+            return;
+        }
+
+        interpolation += speed * Time.deltaTime;
+        transform.position = Vector3.Lerp(startPositionAnimation, endPositionAnimation, interpolation);
+    }
+
+    private void roundPosition()
+    {
         transform.position = new Vector3(
             Mathf.Round(transform.position.x),
             Mathf.Round(transform.position.y),
