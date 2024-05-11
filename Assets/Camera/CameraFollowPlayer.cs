23c23e8b3a7542a6195793f779bed4d8cb0ac8c3
Fatih BOZLAK <bozlak.fatih@gmail.com>
2024-05-11 12:45:43 +0200

    Prefab grass

--- Assets/Camera/CameraFollowPlayer.cs
+++ Assets/Camera/CameraFollowPlayer.cs
@@ -7,22 +7,23 @@ public class CameraFollowPlayer : MonoBehaviour
     private GameObject player;
 
     private float speedMovementCamera = 5f;
+    private float offsetZ = 3f;
 
     private void Start()
     {
         player = GameObject.Find("Player").gameObject;
 
-        transform.position = player.transform.position + Vector3.back * 4 + Vector3.up * 4;
+        transform.position = player.transform.position + Vector3.back * offsetZ + Vector3.up * 4;
         transform.rotation = Quaternion.Euler(30, 0, 0);
     }
 
     private void Update()
     {
         Vector3 targetPositionOfCamera = new Vector3(
-            player.transform.position.x,
-            // transform.position.x,
+            // player.transform.position.x,
+            transform.position.x,
             transform.position.y,
-            player.transform.position.z - 4
+            player.transform.position.z - offsetZ
         );
         transform.position = Vector3.MoveTowards(
             transform.position,
