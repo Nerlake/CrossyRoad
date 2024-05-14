b1acfc81ba95a133c0ad67e9e6237d2340f8d540
Léo HOTZ <cdf.leo.hotz@gmail.com>
2024-05-14 16:21:35 +0200

    Feat: Ajout de bonus vie

---
+++ Assets/Player/PlayerBonus.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBonus : MonoBehaviour
+{
+
+    private LifesContoller lifeController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeController = GameObject.Find("lifes").GetComponent<LifesContoller>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hearth"))
+        {
+            if (lifeController.AddOneLife())
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
--- Assets/UI/LifesContoller.cs
+++ Assets/UI/LifesContoller.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 public class LifesContoller : MonoBehaviour
 {
     private List<Image> lifes = new List<Image>();
+    [SerializeField] private GameObject lifesUI;
+    //private Vector3 defaultHearthPosition;
 
     private void Start()
     {
         lifes.AddRange(transform.GetComponentsInChildren<Image>());
+        //defaultHearthPosition = lifes[0].transform.position;
+        //Debug.Log(defaultHearthPosition.x);
     }
 
     public bool RemoveOneLife()
@@ -20,5 +24,30 @@ public class LifesContoller : MonoBehaviour
         lifes.RemoveAt(lifes.Count - 1);
 
         return true;
+    }
+
+    public bool AddOneLife()
+    {
+        if (lifes.Count >= 3)
+        {
+            Debug.Log("You can't have more than 3 lifes");
+            return false;
+        }
+
+        Vector3 lastHeartPosition = LastHeartPosition();
+        Vector3 newPosition = new Vector3(lastHeartPosition.x - 18, lastHeartPosition.y, lastHeartPosition.z);
+
+        GameObject newLife = Instantiate(lifesUI, newPosition, transform.rotation);
+        newLife.transform.SetParent(transform);
+
+        Debug.Log("Add one life");
+        return true;
+    }
+
+    private Vector3 LastHeartPosition()
+    {
+        if (lifes.Count <= 0) return new Vector3(0,0,0);
+
+        return lifes[lifes.Count - 1].transform.position;
     }
 }
